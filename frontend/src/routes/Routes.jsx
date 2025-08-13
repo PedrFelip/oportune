60d0e1deb2a40e13777b74ef09b718b035e6fd76
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-08-13 09:59:11 -0300

    Instalação do react-router-dom e criação do arquivo das rotas do front end

---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
+const appRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Router></Router>
+            </Routes>
+        </Router>
+    )
+}
+
+export default appRoutes
