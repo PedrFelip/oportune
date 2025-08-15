7abe8c03397245e4dce214e1804c1b152af37b48
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-08-15 18:52:46 -0300

    Ativei o logger do Fastify

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,21 @@
-import Fastify from 'fastify';
+import Fastify from "fastify";
 
-const app = Fastify()
+const app = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid, hostname",
+      },
+    },
+  },
+});
 
-app.get('/', async () => {
-  return { message: 'check' }
-})
+app.get("/", async () => {
+  return { message: "check" };
+});
 
-app.listen({port: 3001}).then(() =>{
-  console.log('rodando na porta 3001')
-})
+app.listen({ port: 3001 }).then(() => {
+  console.log("rodando na porta 3001");
+});
