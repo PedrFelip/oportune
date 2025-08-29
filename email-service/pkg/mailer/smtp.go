00998746b846f3099796e10c5b41b178d8988a83
Pedro Felipe <pfsilva190406@gmail.com>
2025-08-28 23:32:42 -0300

    Adição da função templateProcess em smtp.go para processar templates HTML, permitindo a personalização de emails com dados dinâmicos.

--- email-service/pkg/mailer/smtp.go
+++ email-service/pkg/mailer/smtp.go
@@ -1,13 +1,32 @@
 package mailer
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/pedrfelip/oportune/email-service/internal/models"
 	"gopkg.in/gomail.v2"
 )
+
+func templateProcess(pathTemplate string, data models.TemplateData) (string, error) {
+	var body bytes.Buffer
+	caminho, _ := filepath.Abs(pathTemplate)
+
+	t, err := template.ParseFiles(caminho)
+	if err != nil {
+		return "", fmt.Errorf("falha ao carregar template: %w", err)
+	}
+	if err := t.Execute(&body, data); err != nil {
+		return "", fmt.Errorf("falha ao executar template: %w", err)
+	}
+
+	return body.String(), nil
+}
 
 func tokenConfimacao(userID string) (string, error) {
 	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
