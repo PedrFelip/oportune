3e508496b7cf3bded9f1e20b7a6602cf14d33f9f
Pedro Felipe <pfsilva190406@gmail.com>
2025-08-29 00:10:19 -0300

    Atualização da função Enviar em smtp.go para incluir geração de token de confirmação e envio de e-mail com URL personalizada, além de melhorias na configuração do SMTP utilizando variáveis de ambiente.

--- email-service/pkg/mailer/smtp.go
+++ email-service/pkg/mailer/smtp.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -40,13 +42,43 @@ func tokenConfimacao(userID string) (string, error) {
 	return token.SignedString(jwtSecret)
 }
 
-func Enviar() {
+func Enviar(nome, email, userID string) error {
+	token, err := tokenConfimacao(userID)
+	if err != nil {
+		return fmt.Errorf("falha ao gerar token: %w", err)
+	}
+
+	url := os.Getenv("")
+	urlConfirmacao := fmt.Sprintf("%s/xxxxx?token=%s", url, token)
+
+	template := models.TemplateData{
+		Name:            nome,
+		ConfirmationURL: urlConfirmacao,
+	}
+
+	bodyEmail, err := templateProcess("templates/verificacao_email.html", template)
+	if err != nil {
+		return err
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("SMTP_USER"))
+	m.SetHeader("From", os.Getenv("SMTP_USER"))
+	m.SetHeader("Subject", "Bem-vindo ao Oportune+! Confirme seu e-mail")
+	m.SetBody("text/html", bodyEmail)
+
+	smtpPort, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
 	d := gomail.NewDialer(
 		os.Getenv("SMTP_HOST"),
-		587,
+		smtpPort,
 		os.Getenv("SMTP_USER"),
 		os.Getenv("SMTP_PASS"),
 	)
 
-	fmt.Println(d)
+	if err := d.DialAndSend(m); err != nil {
+		return fmt.Errorf("falha ao enviar email: %w", err)
+	}
+
+	log.Printf("E-mail de ativação enviado com sucesso para %s", email)
+	return nil
 }
