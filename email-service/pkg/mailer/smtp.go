f11163e02981fae9338e4e677a2f469ce5223462
Pedro Felipe <pfsilva190406@gmail.com>
2025-08-28 23:17:29 -0300

    Adição da função tokenConfimacao em smtp.go para gerar tokens JWT para confirmação de conta, utilizando a biblioteca github.com/golang-jwt/jwt/v5.

--- email-service/pkg/mailer/smtp.go
+++ email-service/pkg/mailer/smtp.go
@@ -3,9 +3,23 @@ package mailer
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"gopkg.in/gomail.v2"
 )
+
+func tokenConfimacao(userID string) (string, error) {
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
+	expiracao := time.Now().Add(15 * time.Minute)
+
+	claims := &jwt.RegisteredClaims{
+		Subject:   userID,
+		ExpiresAt: jwt.NewNumericDate(expiracao),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
 
 func Enviar() {
 	d := gomail.NewDialer(
