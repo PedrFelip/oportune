4859bf67cb9894945b254a452e6ac12b26063041
Pedro Felipe <pfsilva190406@gmail.com>
2025-09-10 20:29:16 -0300

    ...

--- backend/src/routes/cnpjRoutes.ts
+++ backend/src/routes/cnpjRoutes.ts
@@ -1,6 +1,6 @@
 
 import { FastifyInstance } from 'fastify';
-import { consultarCnpjController } from '../controllers/cnpjController';
+import { consultarCnpjController } from '../controllers/cnpjController.ts';
 
 export async function cnpjRoutes(app: FastifyInstance) {
   
