86cf73d98091a651ae651f4cbea4b575436117a9
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-09-11 08:58:56 -0300

    .

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,7 @@
 import Fastify from "fastify";
 import { authRoutes } from "./src/routes/authRoutes";
-import { cnpjRoutes } from "./src/routes/cnpjRoutes"; // <--- 1. IMPORTE AS NOVAS ROTAS
+import { cnpjRoutes } from "./src/routes/cnpjRoutes";
 import cors from "@fastify/cors";
-import 'dotenv/config';
 
 const app = Fastify({
   logger: {
@@ -16,19 +15,15 @@ const app = Fastify({
   },
 });
 
-// A sua configuração de CORS já está ótima e vai funcionar para a nova rota.
 await app.register(cors, {
-  origin: ["http://localhost:5173"], // O endereço do seu front-end React/Vite
+  origin: ["http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 } );
 
-// Rotas existentes
 app.register(authRoutes);
 
-// --- ADICIONE A NOVA ROTA AQUI ---
-app.register(cnpjRoutes); // <--- 2. REGISTRE AS NOVAS ROTAS
+app.register(cnpjRoutes);
 
-// Inicia o servidor
 await app.listen({ port: 3001 }).then(() => {
   console.log("Servidor iniciado na porta 3001");
 });
