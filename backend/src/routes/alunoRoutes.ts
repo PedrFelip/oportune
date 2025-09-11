c441f48ccce66efca97b1c72502d054b8f81b68a
Pedro Felipe <pfsilva190406@gmail.com>
2025-09-11 19:14:30 -0300

    feat: update tokenValidator import and add jwt-decode dependency

--- backend/src/routes/alunoRoutes.ts
+++ backend/src/routes/alunoRoutes.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { alunoController } from '../controllers/alunoController.ts'
-import tokenValidator from '../plugins/tokenValidator.ts'
+import Authentication from '../plugins/tokenValidator.ts'
 
 
 
 export async function alunoRoutes(app: FastifyInstance) {
-  app.get('/dashboard', { preHandler: tokenValidator }, alunoController)
+  app.get('/dashboard', { preHandler: Authentication }, alunoController)
 }
