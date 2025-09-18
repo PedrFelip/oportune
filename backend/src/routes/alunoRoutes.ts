390e29b8360111ca5e46ea560b5843f8be74ca8b
Pedro Felipe <pfsilva190406@gmail.com>
2025-09-18 00:07:21 -0300

    Registra rota de vagas no servidor e remove importação desnecessária no módulo de rotas de aluno

--- backend/src/routes/alunoRoutes.ts
+++ backend/src/routes/alunoRoutes.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { alunoController, getPerfilController, getCandidaturasController, getVagasRecomendadasController } from '../controllers/alunoController.ts'
 import Authentication from '../plugins/tokenValidator.ts'
-import prisma from '../../prisma/client.ts'
 
 export async function alunoRoutes(app: FastifyInstance) {
   // Rota para carregamento completo (mantida para compatibilidade)
