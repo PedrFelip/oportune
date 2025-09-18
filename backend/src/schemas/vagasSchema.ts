ad27740fa95058ee57447682ffbeebf1d9654cbd
Pedro Felipe <pfsilva190406@gmail.com>
2025-09-17 23:14:56 -0300

    Adiciona schema de validação para criação de vagas

---
+++ backend/src/schemas/vagasSchema.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+
+export const createVagaSchema = z.object({
+  titulo: z.string().min(2).max(100),
+  descricao: z.string().min(10).max(1000),
+  requisitos: z.array(z.string()).min(1).max(10),
+  tipo: z.enum(['Estágio', 'Pesquisa', 'Extensão']),
+  empresaId: z.string().uuid(),
+  professorId: z.string().uuid(),
+  prazoInscricao: z.date().min(new Date()).optional()
+})
+
+export type VagaCreateDTO = z.infer<typeof createVagaSchema>
