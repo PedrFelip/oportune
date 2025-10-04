e840fe31877562bfb47ae8baa5035aafe790f3ee
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-04 15:02:27 -0300

    Mudanças na tipagem da feature Cadastro

--- frontend/src/features/Cadastro/@types/type.ts
+++ frontend/src/features/Cadastro/@types/type.ts
@@ -1,25 +1,67 @@
-
 export type ProfileType = "ESTUDANTE" | "PROFESSOR" | "EMPRESA";
 
+export type PeriodoType = "MATUTINO" | "VESPERTINO" | "NOTURNO" | null;
+export type GeneroType =
+  | "MASCULINO"
+  | "FEMININO"
+  | "OUTRO"
+  | "PREFIRO NAO DIZER"
+  | null;
+export type CursoType =
+  | "administracao"
+  | "analise_desenvolvimento_sistemas"
+  | "arquitetura_urbanismo"
+  | "ciencia_computacao"
+  | "ciencias_contabeis"
+  | "direito"
+  | "educacao_fisica"
+  | "enfermagem"
+  | "biomedicina"
+  | "farmacia"
+  | "gestao_empresarial"
+  | "gestao_financeira"
+  | "marketing_gestao_clientes"
+  | "saude_publica"
+  | "seguranca_informacao"
+  | "tecnologias_educacao_distancia"
+  | "marketing_digital_ciencia_dados"
+  | "processos_gerenciais"
+  | "terapia_ocupacional"
+  | null;
+
+export type ModalidadeType = "presencial" | "hibrido" | "remoto" | null;
+
+// @types/type.ts
 export interface FormStepProps {
-  // controle de navegação
   onNext?: () => void;
   onBack?: () => void;
 
-  // controle geral
-  handleChange?: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-  ) => void;
-  handleSelectChange?: (name: string, selectedOption: string) => void;
+  formData?: formDataState;
+  setFormData?: React.Dispatch<React.SetStateAction<formDataState>>;
 
-  // dados
-  formData?: FormData;
-  setFormData?: React.Dispatch<React.SetStateAction<FormData>>;
   profileType?: ProfileType | "";
 
-  // usado na primeira etapa
   onProfileSelect?: (type: ProfileType) => void;
 
-  // usado na última etapa
+  handleChange?: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => void;
+  handleSelectChange?: <K extends keyof formDataState>(
+    name: K,
+    selectedOption: formDataState[K]
+  ) => void;
+
   handleFinish?: () => void;
+}
+
+export interface formDataState {
+  nome: string;
+  email: string;
+  senha: string;
+  senhaConfirmada: string;
+  termos: boolean;
+  dataNascimento: Date,
+  genero: GeneroType
 }
