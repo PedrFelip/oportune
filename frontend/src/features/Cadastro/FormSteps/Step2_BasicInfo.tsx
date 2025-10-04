5b9dbe1c738ff90b37ccb28f767bb472f72bc2c4
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-04 16:37:17 -0300

    Passo 2 (aparentemente concluido)

--- frontend/src/features/Cadastro/FormSteps/Step2_BasicInfo.tsx
+++ frontend/src/features/Cadastro/FormSteps/Step2_BasicInfo.tsx
@@ -8,44 +8,18 @@ import {
 } from "@/utils/validadores";
 import { showMessage } from "@/adapters/showMessage";
 // Tipagens
-import { FormStepProps } from "../@types/type";
+import { StepProps } from "../@types/type";
+import { Button } from "@/components/ui/button";
 
 export default function Step2_BasicInfo({
   profileType,
   onNext,
   onBack,
-  formData,
-  handleChange,
-}: FormStepProps) {
-  if (!formData || !handleChange || !onNext || !onBack) return null;
+  register,
+  errors
+}: StepProps) {
 
   const isEmpresa = profileType === "EMPRESA";
-
-  const handleSubmit = (e: React.FormEvent) => {
-    if (!formData) return;
-    e.preventDefault();
-
-    if (!onlyLettersRegex.test(formData.nome)) {
-      showMessage.error("Nome inválido");
-      return;
-    }
-
-    if (!emailRegex.test(formData.email)) {
-      showMessage.error("Email inválido");
-      return;
-    }
-    if (!passwordRegex.test(formData.senha)) {
-      showMessage.error(
-        "A senha precisa de no minimo 8 caracteres, incluindo letras maiúsculas, minúsculas e números"
-      );
-      return;
-    }
-    if (formData.senha !== formData.senhaConfirmada) {
-      showMessage.error("As senhas não conferem");
-      return;
-    }
-    onNext();
-  };
 
   return (
     <div className="animate-fadeIn">
@@ -54,55 +28,46 @@ export default function Step2_BasicInfo({
         subtitle="Vamos começar com as informações básicas de acesso."
       />
       <main>
-        <form onSubmit={handleSubmit}>
           <FormInput
             id="nome"
-            name="nome"
             label={isEmpresa ? "Nome da Empresa" : "Nome Completo"}
             placeholder={
               isEmpresa ? "Ex: Oportune Soluções" : "Ex: João da Silva"
             }
-            value={formData.nome || ""}
-            onChange={handleChange}
-            error={errors}
+            {...register("nome")}
+            error={errors.nome?.message}
           />
           <FormInput
             id="email"
-            name="email"
             label="E-mail"
             type="email"
             placeholder="seuemail@dominio.com"
-            value={formData.email || ""}
-            onChange={handleChange}
+            {...register("email")}
+            error={errors.email?.message}
           />
           <FormInput
             id="senha"
-            name="senha"
             label="Crie uma Senha"
             type="password"
             placeholder="••••••••••"
-            value={formData.senha || ""}
-            onChange={handleChange}
+            {...register("senha")}
+            error={errors.senha?.message}
           />
           <FormInput
             id="senhaConfirmada"
-            name="senhaConfirmada"
             label="Confirme a sua Senha"
             type="password"
             placeholder="••••••••••"
-            value={formData.senhaConfirmada || ""}
-            onChange={handleChange}
+            {...register("senhaConfirmada")}
+            error={errors.senhaConfirmada?.message}
           />
 
           <div className="flex items-center gap-2.5 text-xs mb-4">
             <input
               type="checkbox"
               id="termos"
-              name="termos"
               className="accent-[#2474e4]"
-              required
-              checked={formData.termos || false}
-              onChange={handleChange}
+              {...register("termos")}
             />
             <label htmlFor="termos" className="text-[#c4d3e6]">
               Li e aceito os{" "}
@@ -115,20 +80,20 @@ export default function Step2_BasicInfo({
               .
             </label>
           </div>
-          <button
-            type="submit"
-            className="w-full bg-gradient-to-r from-[#2474e4] to-[#639bec] text-white font-semibold py-3.5 rounded-lg transition-all hover:opacity-90 hover:scale-[1.02] hover:shadow-lg hover:shadow-[#2474e4]/20 mt-2 cursor-pointer"
+          <Button
+            type="button"
+            onClick={onNext}
+            variant={"oportune"}
           >
             Continuar
-          </button>
-          <button
+          </Button>
+          <Button
             type="button"
             onClick={onBack}
-            className="w-full bg-[rgba(196,211,230,0.04)] text-[#c4d3e6] font-medium py-3 rounded-lg border border-white/10 mt-3 transition-all hover:bg-[rgba(196,211,230,0.02)] hover:text-white cursor-pointer"
+            variant={"oportune"}
           >
             Voltar
-          </button>
-        </form>
+          </Button>
       </main>
     </div>
   );
