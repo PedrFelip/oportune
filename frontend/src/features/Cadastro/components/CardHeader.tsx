56f13c4e3a60dbf86d6f7ed3e10ad67ba9cfa204
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-04 15:02:15 -0300

    Criação do cardHeader para formulário

---
+++ frontend/src/features/Cadastro/components/CardHeader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type CardHeaderProps = {
+  showLogo?: boolean,
+  title: string,
+  subtitle: string,
+}
+
+export default function CardHeader({showLogo = false, title, subtitle}: CardHeaderProps) {
+  return (
+    <header className="text-center mb-7">
+      {showLogo && (
+        <div className="flex justify-center items-center gap-3 font-bold mb-4">
+          <div className="w-11 h-11 rounded-lg bg-gradient-to-br from-[#2474e4] to-[#639bec] flex items-center justify-center shadow-lg font-extrabold text-white text-xl">
+            O
+          </div>
+        </div>
+      )}
+      <h1 className="text-white text-2xl font-bold">{title}</h1>
+      <p className="text-sm text-[#c4d3e6] mt-1.5">{subtitle}</p>
+    </header>
+  );
+}
