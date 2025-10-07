fe360ed18c5fb5d2e6b1968017d276068088d020
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-07 11:20:02 -0300

    Adiciona structs para requisição e template de recuperação de senha

--- email-service/internal/models/email.go
+++ email-service/internal/models/email.go
@@ -12,3 +12,17 @@ type TemplateData struct {
 	Email           string
 	ConfirmationURL string
 }
+
+// Request para recuperação de senha
+type RecuperacaoSenhaRequest struct {
+	Name   string `json:"name" binding:"required"`
+	Email  string `json:"email" binding:"required"`
+	UserID string `json:"userID" binding:"required"`
+}
+
+// Template data para recuperação de senha
+type RecuperacaoSenhaTemplateData struct {
+	Name     string
+	Email    string
+	ResetURL string
+}
