5b644317d8f7caa3df78eda08289460ade8237f3
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-07 11:21:17 -0300

    Adiciona rotas para solicitação e redefinição de senha

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,11 +4,15 @@ import {
   loginUsuarioController,
   confirmarEmailController,
   isVerifiedController,
+  solicitarRecuperacaoSenhaController,
+  redefinirSenhaController,
 } from "../controllers/authController.ts";
 
 export async function authRoutes(fastify: FastifyInstance) {
   fastify.post("/createuser", cadastrarUsuarioController);
   fastify.post("/loguser", loginUsuarioController);
   fastify.post("/confirm-email", confirmarEmailController);
-  fastify.post("/is-verified", isVerifiedController)
+  fastify.post("/is-verified", isVerifiedController);
+  fastify.post("/request-password-reset", solicitarRecuperacaoSenhaController);
+  fastify.post("/reset-password", redefinirSenhaController);
 }
