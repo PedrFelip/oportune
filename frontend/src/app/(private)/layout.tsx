5ecff25d3d1c104c18f5f702cadac34c68d6dfd5
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-09 14:46:44 -0300

    Mudanças nos layouts

---
+++ frontend/src/app/(private)/layout.tsx
@@ -0,0 +1,35 @@
+// app/(protected)/layout.tsx
+
+"use client";
+
+import { showMessage } from "@/adapters/showMessage";
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { usuario, carregando } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // A verificação só acontece após o estado de carregamento inicial terminar.
+    if (!carregando && !usuario) {
+      // Se não estiver carregando e não houver usuário, redireciona.
+      router.push("/login");
+    }
+  }, [usuario, carregando, router]);
+
+  if (carregando) {
+    showMessage.loading("Carregando a tela, por favor aguarde!");
+  }
+
+  if (usuario) {
+    return <>{children}</>;
+  }
+
+  return null;
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/global.css";
-import { MessagesContainer } from "@/components/MessageContainer";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MessagesContainer>{children}</MessagesContainer>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
