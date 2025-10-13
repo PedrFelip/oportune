76e1046fcb7fe6209b3c7523a074e4c491d7991a
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-13 14:31:05 -0300

    Adiciona a função getVagaDetalhesService para obter detalhes de uma vaga

--- backend/src/services/vagaServices.ts
+++ backend/src/services/vagaServices.ts
@@ -1,4 +1,4 @@
-import { createVaga, listatodasVagas } from '../repositories/vagasRepository.ts'
+import { createVaga, listatodasVagas, getVagaDetalhes} from '../repositories/vagasRepository.ts'
 
 export const createServiceVaga = async (vagaData: any) => {
   try {
@@ -17,5 +17,15 @@ export const listarServiceVagas = async (page: number, limit: number) => {
   } catch (error) {
     console.error('Erro ao listar vagas:', error);
     throw new Error('Erro ao listar vagas');
+  }
+}
+
+export const getVagaDetalhesService = async (vagaId: string) => {
+  try {
+    const vaga = await getVagaDetalhes(vagaId);
+    return vaga;
+  } catch (error) {
+    console.error('Erro ao obter detalhes da vaga:', error);
+    throw new Error('Erro ao obter detalhes da vaga');
   }
 }
