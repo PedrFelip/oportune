b2be56c6f69c25cfe7ac03b2327ad382c360b1a3
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-13 14:31:16 -0300

    Adiciona rota para obter detalhes de uma vaga

--- backend/src/routes/vagaRoutes.ts
+++ backend/src/routes/vagaRoutes.ts
@@ -1,7 +1,8 @@
-import { createVagaController, listarVagasController } from '../controllers/vagaController.ts'
+import { createVagaController, getVagaDetalhesController, listarVagasController } from '../controllers/vagaController.ts'
 import { FastifyInstance } from 'fastify'
 
 export async function vagaRoutes(app: FastifyInstance) {
   app.post('/vagas', createVagaController)
   app.get('/vagas', listarVagasController)
+  app.get('/vagas/:id', getVagaDetalhesController)
 }
