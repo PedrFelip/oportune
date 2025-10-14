2da12e1864870a5c1f967325458bc46d027890ae
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-14 18:17:06 -0300

    Criação da rota da API de busca vaga pelo ID

---
+++ frontend/src/features/Aluno/api/buscaVagaPeloId.ts
@@ -0,0 +1,26 @@
+export async function buscarVagaPeloId(id: string, token?: string) {
+  try {
+    if (!token) throw new Error("Token não encontrado");
+
+    const response = await fetch(
+      `${process.env.BACKEND_API_URL}/api/aluno/buscar-vaga-pelo-id/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        redirect: "follow",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar vaga");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Erro ao buscar vaga", err);
+    throw err;
+  }
+}
