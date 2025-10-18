e2418bdc1190f23a6aaba44931d8920ca3b68443
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-18 16:57:58 -0300

    Mexi no providers

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { MessagesContainer } from "@/components/MessageContainer";
-import { AuthProvider } from "@/contexts/AuthContext"; // Ajuste o caminho
+import { AuthProvider } from "@/contexts/AuthContext";
+import { LoadingProvider } from "@/contexts/LoadingContext";
 import { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
-      <MessagesContainer>{children}</MessagesContainer>
+      <LoadingProvider>
+        <MessagesContainer>{children}</MessagesContainer>
+      </LoadingProvider>
     </AuthProvider>
   );
 }
