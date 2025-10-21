27f02918781214d00d1ee10893655797acc951cf
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-20 22:14:30 -0300

    Validação da criação de candidatura e adição do serviço de listagem
    Tipo candidaturaData com vagaId e estudanteId.
    
    Retorna um erro se candidaturaVaga falhar.
    
    Adiciona wrapper listarCadidaturasPorEstudanteService.

--- backend/src/services/candidaturaService.ts
+++ backend/src/services/candidaturaService.ts
@@ -1,5 +1,21 @@
-import { candidaturaVaga } from '../repositories/candidaturaRepository.ts'
+import {
+  candidaturaVaga,
+  listarCadidaturasPorEstudante
+} from '../repositories/candidaturaRepository.ts'
 
-export const candidaturaVagaService = async (candidaturaData: any) => {
-  return await candidaturaVaga(candidaturaData)
+export const candidaturaVagaService = async (candidaturaData: {
+  vagaId: string
+  estudanteId: string
+}) => {
+  const candidatura = await candidaturaVaga(candidaturaData)
+
+  if (!candidatura) {
+    return new Error('Erro ao cadastrar candidatura')
+  }
+
+  return candidatura
+}
+
+export const listarCadidaturasPorEstudanteService = async (estudanteId: string) => {
+  return await listarCadidaturasPorEstudante(estudanteId)
 }
