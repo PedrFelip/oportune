0bccaf89ac4f0d6437c6d499336585bf05af095e
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-21 00:28:13 -0300

    .

--- backend/src/@types/fastify.d.ts
+++ backend/src/@types/fastify.d.ts
@@ -1,10 +1,10 @@
 import fastify from "fastify";
 
 declare module "fastify" { // Extensão de interfaces
-    export interface FastifyRequest { // Une isso a interface do FastifyRequest
-        user?: {
-            sub: string // Id do usuário
-            role: "ESTUDANTE" | "PROFESSOR" | "EMPRESA"
-        }
+  export interface FastifyRequest { // Une isso a interface do FastifyRequest
+    user?: {
+      sub: string // Id do usuário
+      role: "ESTUDANTE" | "PROFESSOR" | "EMPRESA"
     }
+  }
 }
