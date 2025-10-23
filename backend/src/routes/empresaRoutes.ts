5ea1d6f88f8818ac9759677ee3f6e1c319980f7e
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-23 16:55:49 -0300

    Refatora as rotas da empresa para utilizar o objeto 'app' e organiza as rotas de 'total-candidaturas'

--- backend/src/routes/empresaRoutes.ts
+++ backend/src/routes/empresaRoutes.ts
@@ -2,9 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { VagasAtivasController } from '../controllers/empresaController.ts'
 import Authentication from '../plugins/tokenValidator.ts'
 
-export default async function empresaRoutes(fastify: FastifyInstance) {
-  fastify.get(
+export default async function empresaRoutes(app: FastifyInstance) {
+  app.get(
     '/dashboard/empresa/vagas-ativas',
+    { preHandler: Authentication },
+    VagasAtivasController,
+  )
+
+  app.get(
+    '/dashboard/empresa/total-candidaturas',
     { preHandler: Authentication },
     VagasAtivasController,
   )
