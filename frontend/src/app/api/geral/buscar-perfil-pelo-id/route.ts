286f5416c12ef327c5284512741a4200c93e1528
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-23 10:31:39 -0300

    Rota de buscar perfil pelo ID

---
+++ frontend/src/app/api/geral/buscar-perfil-pelo-id/route.ts
@@ -0,0 +1,13 @@
+import { proxyRequest } from "@/lib/apiProxy";
+import { NextRequest } from "next/server";
+
+/**
+ * Rota GET para buscar uma vaga específica.
+ * Ela atua como um proxy seguro para o backend real.
+ */
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  return proxyRequest(request, `/profile/${params.id}`);
+}
---
+++ frontend/src/features/Api/buscarPerfilPeloID.ts
@@ -0,0 +1,28 @@
+export async function buscarVagaPeloId(id: string, token?: string) {
+  try {
+    if (!token) throw new Error("Token não encontrado");
+
+    const response = await fetch(
+      `${process.env.BACKEND_API_URL}/geral/buscar-perfil-pelo-id/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        redirect: "follow",
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      console.error("Erro na resposta:", response.status, errorData);
+      throw new Error(`Erro ao buscar vaga: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Erro ao buscar vaga", err);
+    throw err;
+  }
+}
