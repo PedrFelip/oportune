deb831d347497b1e0cd9190e9cb2f282081636b8
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-23 22:41:26 -0300

    Adiciona rota para atualização de vagas

--- backend/src/routes/vagaRoutes.ts
+++ backend/src/routes/vagaRoutes.ts
@@ -2,6 +2,7 @@ import {
   createVagaController,
   getVagaDetalhesController,
   listarVagasController,
+  updateVagaController,
 } from '../controllers/vagaController.ts'
 import { FastifyInstance } from 'fastify'
 
@@ -9,4 +10,5 @@ export async function vagaRoutes(app: FastifyInstance) {
   app.post('/vagas', createVagaController)
   app.get('/vagas', listarVagasController)
   app.get('/vagas/:id', getVagaDetalhesController)
+  app.put('/vagas/:id', updateVagaController)
 }
