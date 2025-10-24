3008b67f4df804c381e512ced2ccc0d75e1a4a5a
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-23 23:14:11 -0300

    Adiciona rota para listar vagas por responsável com autenticação

--- backend/src/routes/vagaRoutes.ts
+++ backend/src/routes/vagaRoutes.ts
@@ -2,13 +2,22 @@ import {
   createVagaController,
   getVagaDetalhesController,
   listarVagasController,
+  listarVagasPorResponsavelController,
   updateVagaController,
 } from '../controllers/vagaController.ts'
 import { FastifyInstance } from 'fastify'
+import Authentication from '../plugins/tokenValidator.ts'
 
 export async function vagaRoutes(app: FastifyInstance) {
   app.post('/vagas', createVagaController)
   app.get('/vagas', listarVagasController)
   app.get('/vagas/:id', getVagaDetalhesController)
   app.put('/vagas/:id', updateVagaController)
+  app.get(
+    '/vagas/responsavel',
+    {
+      preHandler: Authentication
+    },
+    listarVagasPorResponsavelController,
+  )
 }
