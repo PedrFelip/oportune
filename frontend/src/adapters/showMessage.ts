9b0c7d36a97ba58b9d4a6255e98d3cbbd67cd967
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-11-06 08:46:24 -0300

    refactor: Mudança no toast, pra sumir quando aparece um novo

--- frontend/src/adapters/showMessage.ts
+++ frontend/src/adapters/showMessage.ts
@@ -1,11 +1,29 @@
 import { toast } from "react-toastify";
 
 export const showMessage = {
-  success: (msg: string) => toast.success(msg),
-  error: (msg: string) => toast.error(msg),
-  warn: (msg: string) => toast.warn(msg),
-  warning: (msg: string) => toast.warning(msg),
-  info: (msg: string) => toast.info(msg),
-  loading: (msg: string) => toast.loading(msg),
+  success: (msg: string) => {
+    toast.dismiss();
+    toast.success(msg);
+  },
+  error: (msg: string) => {
+    toast.dismiss();
+    toast.error(msg);
+  },
+  warn: (msg: string) => {
+    toast.dismiss();
+    toast.warn(msg);
+  },
+  warning: (msg: string) => {
+    toast.dismiss();
+    toast.warning(msg);
+  },
+  info: (msg: string) => {
+    toast.dismiss();
+    toast.info(msg);
+  },
+  loading: (msg: string) => {
+    toast.dismiss();
+    toast.loading(msg);
+  },
   dismiss: () => toast.dismiss(),
 };
